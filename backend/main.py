d6ee8f374966ee27cdd05973506b92dda8fbc1b2
Siddharth Mohanty <neosiddharth@gmail.com>
2021-12-27 01:04:13 -0800

    Update for pylint

--- backend/main.py
+++ backend/main.py
@@ -9,45 +9,46 @@ APP = flask.Flask(__name__)
 
 
 @APP.route('/list', methods=['GET'])
-def list():
+def list_books():
     """
     list all available books
     """
-    FILE = open('dbs/books.json', 'r')
-    BOOKS = json.load(FILE)
-    FILE.close()
-    return jsonify(BOOKS), 200
+    file = open('dbs/books.json', 'r')
+    book_dict = json.load(file)
+    file.close()
+    return jsonify(book_dict), 200
+
 
 @APP.route('/add', methods=['GET'])
 def add_book():
-    FILE = open('dbs/books.json', 'r')
-    BOOKS = json.load(FILE)
-    FILE.close()
     """
     Add a book to the database
     url/add?data=<code>,<title>,<author>,<section>,<subsection>
     """
+    file = open('dbs/books.json', 'r')
+    book_dict = json.load(file)
+    file.close()
     if 'data' in request.args:
-        DATA = request.args['data']
-        DATA = DATA.split(',')
-        if len(DATA) == 5:
-            CODE = DATA[0]
-            if CODE not in BOOKS:
-                TITLE = DATA[1]
-                AUTHOR = DATA[2]
-                SECTION = DATA[3]
-                SUBSECTION = DATA[4]
-                BOOKS[CODE] = {
+        data = request.args['data']
+        data = data.split(',')
+        if len(data) == 5:
+            code = data[0]
+            if code not in book_dict:
+                title = data[1]
+                author = data[2]
+                section = data[3]
+                subsection = data[4]
+                book_dict[code] = {
                     'available': True,
                     'checked_out_by': 'none',
-                    'title': TITLE,
-                    'author': AUTHOR,
-                    'section': SECTION,
-                    'subsection': SUBSECTION
+                    'title': title,
+                    'author': author,
+                    'section': section,
+                    'subsection': subsection
                 }
-                FILE = open('dbs/books.json', 'w')
-                json.dump(BOOKS, FILE)
-                FILE.close()
+                file = open('dbs/books.json', 'w')
+                json.dump(book_dict, file)
+                file.close()
                 return jsonify({'message': 'Book added'}), 200
             return jsonify({'message': 'Book already exists'}), 400
         return jsonify({'error': 'Missing Data'}), 400
@@ -56,23 +57,23 @@ def add_book():
 
 @APP.route('/checkout', methods=['GET'])
 def checkout():
-    FILE = open('dbs/books.json', 'r')
-    BOOKS = json.load(FILE)
-    FILE.close()
     """
     url/checkout?data=<code>,<user>
     """
+    file = open('dbs/books.json', 'r')
+    book_dict = json.load(file)
+    file.close()
     if 'data' in request.args:
         data = request.args['data']
         data_list = data.split(',')
         if len(data_list) == 2:
-            if data_list[0] in BOOKS:
-                if BOOKS[data_list[0]]['available']:
-                    BOOKS[data_list[0]]['available'] = False
-                    BOOKS[data_list[0]]['checked_out_by'] = data_list[1]
-                    FILE = open('dbs/books.json', 'w')
-                    json.dump(BOOKS, FILE)
-                    FILE.close()
+            if data_list[0] in book_dict:
+                if book_dict[data_list[0]]['available']:
+                    book_dict[data_list[0]]['available'] = False
+                    book_dict[data_list[0]]['checked_out_by'] = data_list[1]
+                    file = open('dbs/books.json', 'w')
+                    json.dump(book_dict, file)
+                    file.close()
                     return jsonify({'message': 'checked_out'}), 200
                 return jsonify({'error': 'Book already checked out'}), 400
             return jsonify({'error': 'Book not found'}), 400
@@ -82,57 +83,69 @@ def checkout():
 
 @APP.route('/return', methods=['GET'])
 def return_book():
-    FILE = open('dbs/books.json', 'r')
-    BOOKS = json.load(FILE)
-    FILE.close()
     """
     url/return?code=<code>
     """
+    file = open('dbs/books.json', 'r')
+    book_dict = json.load(file)
+    file.close()
     if 'code' in request.args:
         code = request.args['code']
-        if not BOOKS[code]['available']:
-            BOOKS[code]['available'] = True
-            BOOKS[code]['checked_out_by'] = 'none'
-            FILE = open('dbs/books.json', 'w')
-            json.dump(BOOKS, FILE)
-            FILE.close()
+        if not book_dict[code]['available']:
+            book_dict[code]['available'] = True
+            book_dict[code]['checked_out_by'] = 'none'
+            file = open('dbs/books.json', 'w')
+            json.dump(book_dict, file)
+            file.close()
             return jsonify({'message': 'returned'}), 200
         return jsonify({'error': 'Book Not Checked Out'}), 400
     return jsonify({'error': 'Missing Data'}), 400
 
 
 @APP.route('/owner', methods=['GET'])
-def owner():
-    FILE = open('dbs/books.json', 'r')
-    BOOKS = json.load(FILE)
-    FILE.close()
+def get_owner():
     """
     url/owner?code=<code>
     """
+    file = open('dbs/books.json', 'r')
+    book_dict = json.load(file)
+    file.close()
     if 'code' in request.args:
         code = request.args['code']
-        if code in BOOKS:
-            owner = BOOKS[code]['checked_out_by']
+        if code in book_dict:
+            owner = book_dict[code]['checked_out_by']
             if owner != 'none':
                 return jsonify({'message': owner}), 200
             return jsonify({'error': 'Book Not Checked Out'}), 400
         return jsonify({'error': 'Book Not Found'}), 400
     return jsonify({'error': 'Missing Data'}), 400
 
+
 @APP.route('/search', methods=['GET'])
 def search():
-    FILE = open('dbs/books.json', 'r')
-    BOOKS = json.load(FILE)
-    FILE.close()
     """
-    url/search?data=<title>
+    url/search?data=<field>,<info>
     """
+    file = open('dbs/books.json', 'r')
+    book_dict = json.load(file)
+    file.close()
     if 'data' in request.args:
         data = request.args['data']
-        if data in BOOKS:
-            return jsonify({'message': 'found'}), 200
-        return jsonify({'error': 'Book Not Found'}), 400
+        data = data.split(',')
+        if len(data) == 2:
+            field = data[0]
+            info = data[1]
+            if field in ['section', 'subsection', 'title', 'isbn', 'author']:
+                ret_list = []
+                for book in book_dict:
+                    if book_dict[book][field] == info:
+                        ret_list.append(book_dict[book][field])
+                return jsonify(ret_list), 200
+            return jsonify({'error': 'Bad field'}), 400
+        return jsonify({'error': 'Missing Data'}), 400
     return jsonify({'error': 'Missing Data'}), 400
+
+
 # @APP.route('/', methods=['GET'])
 # @APP.route('/', methods=['GET'])
 # @APP.route('/', methods=['GET'])
