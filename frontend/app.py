ddc43c77eca9a1e37547bb4d22fc38f090651434
Siddharth Mohanty <neosiddharth@gmail.com>
2022-01-25 17:14:09 -0800

    Error Handling

--- frontend/app.py
+++ frontend/app.py
@@ -7,10 +7,10 @@ from flask import Flask, render_template, request
 APP = Flask(__name__)
 
 
-@APP.route('/search')
-def search():
+@APP.route('/result')
+def result():
     """
-    Search
+    Search Result
     """
     search_type = str(request.args.get('search_type')).lower().strip()
     search_term = str(request.args.get('search_term')).lower().strip()
@@ -19,10 +19,63 @@ def search():
         status = get(
             f'http://localhost:5000/search?field={search_type}&info={search_term}'
         ).json()
-        print(status)
         if 'error' not in status:
-            return render_template('search.html', status=status['message'][0]['title'])
+            response_str = []
+            status = status['message']
+            print(status)
+            for book in status:
+                response_str.append(book['title'])
+            if len(response_str) > 0:
+                return render_template('result.html', status=response_str)
+            return render_template('result.html', status=["No Books Found"])
+        print(status['error'])
     return f"{search_type} is not valid try: {search_types}"
+
+
+@APP.route('/search')
+def search():
+    """
+    Book Search
+    """
+    return render_template('search.html')
+
+
+@APP.route('/add')
+def add():
+    """
+    Add books
+    """
+    return render_template('add.html')
+
+
+@APP.route('/list')
+def list_books():
+    """
+    List books
+    """
+    books = get('http://localhost:5000/list').json()
+    book_list = []
+    for book in books:
+        book_list.append(f"{book} {books[book]['title']}")
+    return render_template('list.html', book_list=book_list)
+
+
+@APP.route('/addtodb', methods=['POST'])
+def add_book():
+    """
+    Add book to database
+    """
+    code = request.form.get('code')
+    title = request.form.get('title')
+    author = request.form.get('author')
+    section = request.form.get('section')
+    subsection = request.form.get('subsection')
+    status = get(
+        f'http://localhost:5000/add?data={code},{title},{author},{section},{subsection}'
+    ).json()
+    if 'error' not in status:
+        return render_template('add.html', message=status['message'])
+    return render_template('add.html', error=status['error'])
 
 
 @APP.route('/')
